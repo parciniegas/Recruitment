0ab45c3aeec17eaafa4001852f8bf26cac499dc7
Pedro Arciniegas Diaz <parciniegas@hotmail.com>
2022-08-16 14:36:03 -0500

    add EntityAlreadyExistException to version control

---
+++ Sequences/Data/Exceptions/EntityAlreadyExistException.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace Sequences.Data.Exceptions
+{
+#pragma warning disable S3925 // "ISerializable" should be implemented correctly
+
+    public class EntityAlreadyExistException : Exception
+    {
+        public EntityAlreadyExistException()
+        {
+        }
+
+        public EntityAlreadyExistException(string? message) : base(message)
+        {
+        }
+
+        public EntityAlreadyExistException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected EntityAlreadyExistException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+#pragma warning restore S3925 // "ISerializable" should be implemented correctly
+}
